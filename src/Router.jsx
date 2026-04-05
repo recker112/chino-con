9abd10d306352ec00f3bb80d6d943b0597d64fb9
Recker <joseortiz112001@gmail.com>
2026-04-04 21:42:55 -0400

    feat: agregar componente Router con configuración de rutas y carga diferida

---
+++ src/Router.jsx
@@ -0,0 +1,53 @@
+/**
+ * React
+ */
+import { lazy, Suspense } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+/**
+ * Components
+ */
+import Fallback from "./components/routers/Fallback";
+import ErrorFallback from "./components/routers/ErrorFallback";
+
+/**
+ * Osys base
+ */
+const DefaultLayout = lazy(() => import("./components/routers/DefaultLayout"));
+const Home = lazy(() => import("./pages/Home"));
+
+const router = createBrowserRouter([
+  /* Branch Público */
+  {
+    path: "/",
+    errorElement: <ErrorFallback />,
+    element: <DefaultLayout />,
+    children: [
+      {
+        index: true,
+        element: (
+          <Home />
+        ),
+      },
+      {
+        path: "*",
+        element: <Fallback text="Página no encontrada" homeButton />,
+      },
+    ],
+  },
+]);
+
+export default function Router() {
+  return (
+    <Suspense
+      fallback={
+        <Fallback
+          text="Estamos preparando todo para ti..."
+          loading
+        />
+      }
+    >
+      <RouterProvider router={router} />
+    </Suspense>
+  );
+}
