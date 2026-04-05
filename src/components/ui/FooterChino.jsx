a86f1451d4e30781cf9de5cf23ea0d4e76012605
Recker <joseortiz112001@gmail.com>
2026-04-04 22:57:41 -0400

    feat: agregar componente FooterChino

---
+++ src/components/ui/FooterChino.jsx
@@ -0,0 +1,13 @@
+import { Box, Divider, Typography } from '@mui/material'
+import React from 'react'
+
+export default function FooterChino() {
+  return (
+    <Box>
+      <Divider />
+      <Typography variant="caption" component="div" color="textSecondary" align="center" sx={{ py: 3 }}>
+        Hecho con amor para aprender chino
+      </Typography>
+    </Box>
+  )
+}
