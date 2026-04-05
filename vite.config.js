199d609c920f4cd4c5b24c0683eb80336033f20a
Recker <joseortiz112001@gmail.com>
2026-04-04 20:21:47 -0400

    feat: agregar configuración de construcción y opciones de salida en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,20 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import eslintPlugin from "@nabla/vite-plugin-eslint";
-import mkcert from 'vite-plugin-mkcert';
+import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    emptyOutDir: true, // also necessary
+    rollupOptions: {
+      output: {
+        entryFileNames: "index.[hash].js", // Agregar hash al nombre del archivo
+        chunkFileNames: "assets/[name].[hash].js", // Agregar hash a los chunks
+        assetFileNames: "assets/[name].[hash].[ext]",
+      },
+    },
+  },
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
