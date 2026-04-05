6999eaa5aa6f3a18f43a723f3f1f084c9d768af8
Recker <joseortiz112001@gmail.com>
2026-04-05 00:29:23 -0400

    feat: agregar componente TabContentSelector para la selección de fonemas

---
+++ src/pages/home/Components/TabContentSelector.jsx
@@ -0,0 +1,73 @@
+import { Grid, Paper, Typography } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+
+import { content } from "./Content";
+import { updateUserConfig } from "../../../store/reducers/user/configReducer";
+
+export default function TabContentSelector() {
+  const tabFonema = useSelector((state) => state.userConfig.tabFonema);
+  const fonemaSelected = useSelector(
+    (state) => state.userConfig.fonemaSelected,
+  );
+  const dispatch = useDispatch();
+
+  const handleSelected = (item) => {
+    const id = item.title + item.chino;
+    const fonemaId = fonemaSelected?.title + fonemaSelected?.chino;
+    if (fonemaId === id) {
+      return null;
+    }
+
+    dispatch(
+      updateUserConfig({ key: "fonemaSelected", value: item, notSave: true }),
+    );
+  };
+
+  return (
+    <>
+      <Typography component="div" color="textSecondary" sx={{ mt: 4 }}>
+        Contenido de la Lista {tabFonema + 1}
+      </Typography>
+      <Grid container spacing={2} sx={{ mt: 1, mb: 4 }}>
+        {content[tabFonema]?.map((item, index) => (
+          <Grid size={{ xs: 4, sm: 3, md: 1.5 }} key={index}>
+            <Paper
+              elevation={3}
+              sx={{
+                p: 2,
+                textAlign: "center",
+                cursor: "pointer",
+                borderColor:
+                  fonemaSelected?.title + fonemaSelected?.chino ===
+                  item.title + item.chino
+                    ? "primary.main"
+                    : "transparent",
+                borderWidth: 2,
+                borderStyle: "solid",
+                borderRadius: 2,
+                "&:hover": {
+                  borderColor: "primary.main",
+                  transform: "scale(1.05)",
+                },
+                transition: "transform 0.2s, border-color 0.2s",
+              }}
+              onClick={() => handleSelected(item)}
+            >
+              <Typography
+                variant="h5"
+                fontWeight="bold"
+                color="primary"
+                component="div"
+              >
+                {item.title}
+              </Typography>
+              <Typography variant="body1" component="div" color="textSecondary">
+                {item.chino}
+              </Typography>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
+}
