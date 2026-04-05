6b9386b14bdc3fe28968e81d30b8dce28ee2c503
Recker <joseortiz112001@gmail.com>
2026-04-05 00:29:19 -0400

    feat: agregar contenido inicial en el componente Content

---
+++ src/pages/home/Components/Content.js
@@ -0,0 +1,11 @@
+export const content = [
+  [
+    {
+      title: "b",
+      chino: "波",
+      subchino: "时间",
+      subchinoPinyin: "(shíjiān)",
+      subchinoTranslation: "tiempo"
+    },
+  ],
+];
