f3919a5aa76a818ca9a8c44b624f89c4e25737e4
Recker <joseortiz112001@gmail.com>
2026-04-04 22:57:44 -0400

    fix: reorganizar el componente Home para incluir FooterChino

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,19 @@
 import { Typography } from "@mui/material";
 import ContainerChino from "../../components/ui/ContainerChino";
+import FooterChino from "../../components/ui/FooterChino";
 
 export default function Home() {
   return (
-    <ContainerChino>
-      <Typography variant="h6" fontWeight="bold" component="div">
-        Selecciona un fonema
-      </Typography>
-      <Typography variant="body" component="div" color="textSecondary">
-        Haz clic en cualquier fonema para escuchar su pronunciación
-      </Typography>
-    </ContainerChino>
-  )
+    <>
+      <ContainerChino>
+        <Typography variant="h6" fontWeight="bold" component="div">
+          Selecciona un fonema
+        </Typography>
+        <Typography variant="body" component="div" color="textSecondary">
+          Haz clic en cualquier fonema para escuchar su pronunciación
+        </Typography>
+      </ContainerChino>
+      <FooterChino />
+    </>
+  );
 }
