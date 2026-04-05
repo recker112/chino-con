8d3ea1a7888344307c7b71e881ce375f3750d547
Recker <joseortiz112001@gmail.com>
2026-04-04 21:43:06 -0400

    feat: implementar botón para cambiar el tema en la página de inicio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,17 @@
+import { Box, Button } from "@mui/material";
+import { updateTheme } from "../store/reducers/user/configReducer";
+import { useDispatch } from "react-redux";
+
 export default function Home() {
+  const dispatch = useDispatch();
+
   return (
-    <div>Home</div>
+    <Box>
+      <Button variant="contained" color="primary" onClick={() => {
+        dispatch(updateTheme());
+      }}>
+        Cambiar tema
+      </Button>
+    </Box>
   )
 }
