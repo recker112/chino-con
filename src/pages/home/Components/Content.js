b8ca27eadee95c78a032dcaa6cd86f472d2d18f4
Recker <joseortiz112001@gmail.com>
2026-04-05 01:36:26 -0400

    feat: agregar audio de fonemas a los elementos de contenido

--- src/pages/home/Components/Content.js
+++ src/pages/home/Components/Content.js
@@ -1,3 +1,5 @@
+import b波 from "../../../assets/fonemas/b波.m4a";
+
 export const content = [
   [
     {
@@ -5,7 +7,32 @@ export const content = [
       chino: "波",
       subchino: "时间",
       subchinoPinyin: "(shíjiān)",
-      subchinoTranslation: "tiempo"
+      subchinoTranslation: "tiempo",
+      audio: b波
+    },
+    {
+      title: "x",
+      chino: "波",
+      subchino: "时间",
+      subchinoPinyin: "(shíjiān)",
+      subchinoTranslation: "tiempo",
+      audio: b波
+    },
+    {
+      title: "y",
+      chino: "波",
+      subchino: "时间",
+      subchinoPinyin: "(shíjiān)",
+      subchinoTranslation: "tiempo",
+      audio: b波
+    },
+    {
+      title: "xd",
+      chino: "波",
+      subchino: "时间",
+      subchinoPinyin: "(shíjiān)",
+      subchinoTranslation: "tiempo",
+      audio: b波
     },
   ],
 ];
